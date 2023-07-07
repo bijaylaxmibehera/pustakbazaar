2f28944316459412fed64262375dcea44b32a222
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2023-07-07 08:31:03 +0530

    style: add style to wishlist page

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -8,30 +8,33 @@ import { removedFromWishlist } from "../../services/WishlistService";
 import { NavLink } from "react-router-dom";
 
 export function Wishlist() {
-  const { wishlist,cart,dataDispatch } = useContext(DataContext);
-  const {token}=useContext(AuthContext);
+  const { wishlist, cart, dataDispatch } = useContext(DataContext);
+  const { token } = useContext(AuthContext);
 
- const handleRemoveFromWishlist=(product)=>{
-    removedFromWishlist(dataDispatch, product._id, token)
- }
+  const handleRemoveFromWishlist = (product) => {
+    removedFromWishlist(dataDispatch, product._id, token);
+  };
 
-  const handleMoveToCart=(product)=>{
+  const handleMoveToCart = (product) => {
     addToCart(dataDispatch, product, token);
-    removedFromWishlist(dataDispatch, product._id, token)
-  }
+    removedFromWishlist(dataDispatch, product._id, token);
+  };
 
   return (
     <>
-      <h3 style={{ textAlign: 'center' }}>
-        My wishlist({wishlist.length})
-      </h3>
+      <div className="wishlist-heading-text">
+        <h3>My wishlist({wishlist.length})</h3>
+      </div>
       {wishlist.length === 0 ? (
-        <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"70vh", fontWeight:"var(--font-semiBold)", fontSize:"1.2rem"}}>
-        <p>Your wishlist is empty</p>
+        <div className="empty-wishlist">
+          <p>Your wishlist is empty</p>
+          <NavLink to="/products">
+            <button className="btn empty-wishlist-btn">Add now</button>
+          </NavLink>
         </div>
       ) : (
-        <div className='responsive-grid'>
-          {wishlist.map(item => {
+        <div className="responsive-grid">
+          {wishlist.map((item) => {
             const {
               _id: id,
               img,
@@ -40,40 +43,40 @@ export function Wishlist() {
               price,
               originalPrice,
               isBestSeller,
-              rating
-            } = item
+              rating,
+            } = item;
             return (
-              <div key={id} className='card'>
-                <img className='card-img' src={img} alt={name} />
+              <div key={id} className="card">
+                <img className="card-img" src={img} alt={name} />
                 {isBestSeller && (
-                  <span className='card-badge'>Best Seller</span>
+                  <span className="card-badge">Best Seller</span>
                 )}
                 <span
-                  role='button'
-                  className='wishlist-icon'
+                  role="button"
+                  className="wishlist-icon"
                   onClick={() => handleRemoveFromWishlist(item)}
-                  style={{ color: 'var(--danger)' }}
+                  style={{ color: "var(--danger)" }}
                 >
-                  <i className='fa fa-heart' aria-hidden='true'></i>
+                  <i className="fa fa-heart" aria-hidden="true"></i>
                 </span>
-                <div className='card-info'>
-                  <div className=''>
-                    <div className='card-title'>
-                      <h3 className='card-title-header' title={name}>
+                <div className="card-info">
+                  <div className="">
+                    <div className="card-title">
+                      <h3 className="card-title-header" title={name}>
                         {name}
                       </h3>
-                      <div className='card-star'>
+                      <div className="card-star">
                         <p>{rating}</p>
-                        <i class='fa fa-star' aria-hidden='true'></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
                       </div>
                     </div>
-                    <p className='card-description'>{author}</p>
+                    <p className="card-description">{author}</p>
                   </div>
-                  <div className='price'>
-                    <p className='disc-price'>₹{price}</p>
-                    <p className='actual-price'>₹{originalPrice}</p>
+                  <div className="price">
+                    <p className="disc-price">₹{price}</p>
+                    <p className="actual-price">₹{originalPrice}</p>
                   </div>
-                  <p className='price-percentage'>
+                  <p className="price-percentage">
                     <CalculateDiscount
                       price={price}
                       originalPrice={originalPrice}
@@ -82,26 +85,27 @@ export function Wishlist() {
                   </p>
                 </div>
                 {isProductInCart(cart, id) ? (
-                     <NavLink to='/cart'>
-                     <button
-                       className='btn default add-cart'
-                       style={{ backgroundColor: 'var(--link)' }}
-                     >
-                       Already in cart
-                     </button>
-                   </NavLink>
-                ):(<button
-                    className='btn default add-cart'
+                  <NavLink to="/cart">
+                    <button
+                      className="btn default add-cart"
+                      style={{ backgroundColor: "var(--link)" }}
+                    >
+                      Already in cart
+                    </button>
+                  </NavLink>
+                ) : (
+                  <button
+                    className="btn default add-cart"
                     onClick={() => handleMoveToCart(item)}
                   >
                     Move to cart
-                  </button>)}
+                  </button>
+                )}
               </div>
-            )
+            );
           })}
         </div>
       )}
     </>
-  )
+  );
 }
-
