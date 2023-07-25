58e89ce53459454438df1c7acb710a07c3916be6
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2023-07-25 21:13:36 +0530

    style: minor change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,15 +28,20 @@ export function Home() {
                 alt=""
               />
             </div>
-            <div className="main-text">
-              <p>Welcome to pustakbazaar!</p>
-              <h1>For all book lovers, a one-stop online shop</h1>
-              <button className="btn secondary shop-btn">
-                <NavLink to="/products">
-                  Browse Collection
-                  <i class="fa fa-chevron-right" aria-hidden="true"></i>
-                </NavLink>
-              </button>
+            <div className="hero-section">
+              <div className="main-text">
+                <p>Welcome to pustakbazaar!</p>
+                <h1>For all book lovers, a one-stop online shop</h1>
+                <button className="btn secondary shop-btn">
+                  <NavLink to="/products">
+                    Browse Collection
+                    <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                  </NavLink>
+                </button>
+              </div>
+              {/* <div className="hero-img">
+                <img src="../../assets/undraw_book_lover_re_rwjy.svg" alt="hero image"/>
+              </div> */}
             </div>
           </main>
           <section className="available-category">
@@ -56,24 +61,40 @@ export function Home() {
             </div>
           </section>
           <section className="quality-service">
-            <h1>We provide best customer experience</h1>
+            <div className="service-heading">
+              <h1>We provide best customer experience</h1>
+              <small>
+                We ensure our customers have best shopping experience
+              </small>
+            </div>
+
             <div className="responsive-grid quality">
               <div>
                 <TaskAltIcon color="success" />
-                <p>Best quality paper</p>
+                <h4>Best quality paper</h4>
+                <p>
+                  We provide money back guarentee if the paper quality are not
+                  good
+                </p>
               </div>
               <div>
                 <SwapHorizIcon color="secondary" />
                 {/* <i class="fa fa-exchange" aria-hidden="true"></i> */}
-                <p>Easy return within 7 days</p>
+                <h4>Easy return within 7 days</h4>
+                <p>
+                  We provide easy return policy if you are not satisfy with
+                  product quality
+                </p>
               </div>
               <div>
                 <PhoneCallbackIcon color="success" />
-                <p>24 X 7 customer support</p>
+                <h4>24 X 7 customer support</h4>
+                <p>We are available to answer your query</p>
               </div>
               <div>
                 <i class="fa fa-truck" aria-hidden="true"></i>
-                <p>Fast delivery</p>
+                <h4>Fast delivery</h4>
+                <p>We offer fast and free shipping for our loyal customer</p>
               </div>
             </div>
           </section>
